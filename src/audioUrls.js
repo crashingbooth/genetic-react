43f1cfb28b640ed70a10882ded097e27f6e5df53
crashing booth <jholtzkener@hotmail.com>
2022-04-20 09:45:46 -0400

    changes audio resources

--- src/audioUrls.js
+++ src/audioUrls.js
@@ -1,34 +1,34 @@
 import * as Tone from "tone";
 
-const resourceBaseUrl = "https://raw.githubusercontent.com/crashingbooth/static-step-sequencer/main/"
+const resourceBaseUrl = "https://raw.githubusercontent.com/crashingbooth/simple-percussion/main/"
 const resources = {
   kick1: {
-    filename: "audio/kick.mp3",
+    filename: "lo/001.wav",
     displayName: "kick 1",
     note: "C3"
   },
   kick2: {
-    filename: "audio/kick1.wav",
+    filename: "lo/002.wav",
     displayName: "kick 2",
     note: "D3"
   },
   hh1: {
-    filename: "audio/hh.wav",
+    filename: "lo/003.wav",
     displayName: "hh 1",
     note: "E3"
   },
   rim1: {
-    filename: "audio/rim2.wav",
+    filename: "lo/004.wav",
     displayName: "rim 1",
     note: "F3"
   },
   tom1: {
-    filename: "audio/tom.wav",
+    filename: "lo/005.wav",
     displayName: "tom 1",
     note: "G3"
   },
   snare1: {
-    filename: "audio/snare3.wav",
+    filename: "lo/006.wav",
     displayName:  "snare 1",
     note: "A3"
   }
